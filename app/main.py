9fd061edb37a66a5c65e81ebc2f7fcdd0fa96a90
Ahmad <ahmad.al-jabbouri@thalesgroup.com>
2026-06-04 12:53:23 -0400

    checkpoint

--- app/main.py
+++ app/main.py
@@ -1,17 +1,65 @@
-try:
-    from fastapi import FastAPI
-    
-    app = FastAPI()
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from datetime import datetime, date, time, timedelta
+from decimal import Decimal
+from uuid import UUID
+from enum import Enum
+from typing import List, Dict, Set, Optional, Literal, Union
+from ipaddress import IPv4Address
 
+app = FastAPI()
+
+class Status(str, Enum):
+    ACTIVE = "active"
+    INACTIVE = "inactive"
+
+class Metadata(BaseModel):
+    created: datetime
+    version: int = 1
+
+class Event(BaseModel):
+    # Primitives
+    id: UUID
+    summary: str
+    description: Optional[str] = None
+    is_all_day: bool = False
+    
+    # Numbers
+    sequence: int = Field(ge=0)
+    duration: float = Field(gt=0)
+    cost: Decimal = Decimal("0.00")
+    
+    # Temporal
+    start: datetime
+    end: datetime
+    reminder: Optional[time] = None
+    offset: timedelta
+    
+    # Collections
+    attendees: List[str] = []
+    categories: Set[str] = set()
+    properties: Dict[str, str] = {}
+    
+    # Special
+    status: Status = Status.ACTIVE
+    visibility: Literal["public", "private"] = "private"
+    server_ip: IPv4Address
+    
+    # Nested
+    metadata: Metadata
+
+    @app.post("/events")
+    async def create(event: Event):
+        return event
 
     @app.get("/")
-    def read_root():
+    async def read_root():
         return {"Hello": "World"}
-
 
     @app.get("/items/{item_id}")
     def read_item(item_id: int, q: str | None = None):
         return {"item_id": item_id, "q": q}
-
-except Exception as e:
-    print(e)
+    
+    @app.get("/health")
+    def check_health():
+        return "u good my boi"
